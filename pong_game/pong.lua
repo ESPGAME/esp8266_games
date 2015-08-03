8f6208c73a5070bc9f2c06a49c26dc35088b80dc
Alexander <sash13@bigmir.net>
2015-08-03 18:09:20 +0900

    Redesign pong class

--- pong_game/pong.lua
+++ pong_game/pong.lua
@@ -1,67 +1,8 @@
--- from http://loadingdata.nl/lua-classes/
-function Class(...)  
-  local cls  = {}
-  local inst = { [cls] = true }
-
-  for _, base in ipairs({...}) do
-    inst[base] = true
-    for k, v in pairs(base) do
-      cls[k] = v
-    end
-  end
-
-  cls.__index = cls
-  cls.instance_of = inst
-
-  setmetatable(cls, {
-    __call = function(c, ...)
-      local instance = setmetatable({}, c)
-      if instance.__init then
-        instance:__init(...)
-      end
-      return instance
-    end
-  })
-
-  return cls
-end  
-
-
-Paddle = {}  
-Paddle = Class({  
-  __init = function(self, x, y)
-    self.x, self.y = x, y
-    self.width, self.height = 2, 20
-    self.score = 0
-  end,
-  
-  draw = function(self)
-    disp:drawBox(self.x, self.y, self.width, self.height)
-  end,
-
-  -- etc
-})
-
-Ball = {}  
-Ball = Class({  
-  __init = function(self, x, y)
-    self.x, self.y = x, y
-    self.vx, self.vy = 2, 2
-    self.r = 2
-  end,
-  update = function(self)
-    self.x = self.x + self.vx
-    self.y = self.y + self.vy
-    -- print(self.x, self.y)
-  end, 
-  
-  draw = function(self)
-    disp:drawDisc(self.x, self.y, self.r)
-    --disp:drawPixel(self.x, self.y)
-  end,
-
-  -- etc
-})
+obj = require("pong_objects")
+local BALL_R = 2
+local PAD_W = 2
+local PAD_H = 20
+local ONE_SEC_US = 1000000
 
 function init_i2c_display()
      -- SDA and SCL can be assigned freely to available GPIOs
@@ -75,44 +16,50 @@ end
 function init_game()
     height = disp:getHeight()
     width = disp:getWidth()
-    first_player = Paddle(2, 0)
-    first_player.y = height/2 - first_player.height/2
-    second_player = Paddle(width - 2 - 2, 0)
-    second_player.y = height/2 - first_player.height/2
+    first_player = obj.Paddle(PAD_W, PAD_H)
+    first_player.x = 2
+    first_player.y = height/2 - first_player.size[2]/2
+    second_player = obj.Paddle(PAD_W, PAD_H)
+    second_player.x = width - 2 - 2
+    second_player.y = height/2 - second_player.size[2]/2
 
-    ball = Ball(width/2, height/2)
-
+    ball = obj.Ball(BALL_R)
+    ball.x = width/2
+    ball.y = height/2
+    ball.direction = {1, 1}
     loopcnt = 0
-    old = tmr.time()
+    old = tmr.now()
     now = 0
     fps = 0
 end
 
 function update_game()
-   now = tmr.time()
-   if now - old >= 1 then
-    --print(old)
-    old = now
-    first_player.score = fps
-    fps = 0 
-   end
-   loopcnt = loopcnt + 1
-   if loopcnt > 50 then
-    tmr.stop(1)
-   end
-    ball.update(ball)
-    if (ball.x >  width-ball.r*2 or ball.x - ball.r < 0) then
-        ball.vx = - ball.vx
-    elseif (ball.y >  height-ball.r*2 or ball.y - ball.r < 0) then
-        ball.vy = - ball.vy
+    now = tmr.now()
+    local dt = now - old
+    --print(dt)
+    if dt >= ONE_SEC_US then
+        old = now
+        first_player.score = fps
+        fps = 0 
     end
+    loopcnt = loopcnt + 1
+    if loopcnt > 50 then
+        tmr.stop(1)
+    end
+    ball.update(1)
+    if (ball.x >  width-ball.r*2 or ball.x - ball.r < 0) then
+        ball.direction[1] = - ball.direction[1]
+    elseif (ball.y >  height-ball.r*2 or ball.y - ball.r < 0) then
+        ball.direction[2] = - ball.direction[2]
+    end
+    --print('update:', tmr.now()-now)
 end
 
 function draw_game()
-    first_player.draw(first_player)
-    second_player.draw(second_player)
+    first_player.draw()
+    second_player.draw()
     -- ball
-    ball.draw(ball)
+    ball.draw()
     -- score
     disp:setFont(u8g.font_6x10)
     disp:drawStr(width/2 - 10, height/8, tostring(first_player.score))
@@ -126,33 +73,20 @@ function draw_game()
     end
 end
 
-function game_loop()
-    local loopcnt
-    fps = 0
-    for loopcnt = 1, 200, 1 do
-        disp:firstPage()
-        update_game()
-        repeat
-            draw_game()
-        until disp:nextPage() == false
-        tmr.delay(20)
-        fps = fps +1
-        tmr.wdclr()
-    end
-end
-
 init_i2c_display()
 init_game()
---game_loop()
 
-tmr.alarm(1, 500, 1, function() 
+tmr.alarm(1, 5, 1, function() 
    update_game()
+   
+   --local now_draw = tmr.now()
    disp:firstPage()
    repeat
     draw_game()
    until disp:nextPage() == false
-   --if disp:nextPage() then draw_game() end
+   --print("upd", tmr.now() - now_draw)
    
    tmr.wdclr()
    fps = fps +1
+
 end )
---
+++ pong_game/pong_objects.lua
@@ -0,0 +1,47 @@
+local moduleName = ...
+local M = {}
+_G[moduleName] = M
+
+local function Moveable()
+    local self = {
+        x = 0,
+        y = 0,
+        speed = 2,
+        direction = {0, 0}
+    }
+    function self.update()
+    end
+    function self.draw()
+    end
+    return self
+end
+
+local function Paddle(w, h)
+    local self = Moveable()
+    self.size = {w, h}
+    self.score = 0
+
+    function self.draw()
+        disp:drawBox(self.x, self.y, self.size[1], self.size[2])
+    end
+    
+    return self
+end
+
+local function Ball(r)
+    local self = Moveable()
+    self.r = r
+    function self.update(dt)
+        self.x = self.x + self.speed*self.direction[1]*dt
+        self.y = self.y + self.speed*self.direction[2]*dt
+    end
+    function self.draw()
+        disp:drawDisc(self.x, self.y, self.r)
+    end
+    return self
+end
+
+M.Moveable = Moveable
+M.Paddle = Paddle
+M.Ball = Ball
+return M
