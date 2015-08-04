d005ce187187f4a270f802873da2b8ccd504c671
Alexander <sash13@bigmir.net>
2015-08-04 16:13:09 +0900

    Update style & add paddle physics

--- pong_game/pong.lua
+++ pong_game/pong.lua
@@ -4,121 +4,123 @@ local PAD_W = 2
 local PAD_H = 20
 local ONE_SEC_US = 1000000
 
-local TEST_LOOPS = 200
+local FPS = 25
+local TICKS = 1000/FPS
+
+local TEST_LOOPS = 50
 
 function init_i2c_display()
-     -- SDA and SCL can be assigned freely to available GPIOs
-     local sda = 5 -- GPIO14
-     local scl = 6 -- GPIO12
-     local sla = 0x3c
-     i2c.setup(0, sda, scl, i2c.SLOW)
-     disp = u8g.ssd1306_128x64_i2c(sla)
+  -- SDA and SCL can be assigned freely to available GPIOs
+  local sda = 5 -- GPIO14
+  local scl = 6 -- GPIO12
+  local sla = 0x3c
+  i2c.setup(0, sda, scl, i2c.SLOW)
+  disp = u8g.ssd1306_128x64_i2c(sla)
 end
 
 function init_game()
-    height = disp:getHeight()
-    width = disp:getWidth()
-    first_player = obj.Paddle(PAD_W, PAD_H)
-    first_player.x = 2
-    first_player.y = height/2 - first_player.size[2]/2
-    second_player = obj.Paddle(PAD_W, PAD_H)
-    second_player.x = width - 2 - 2
-    second_player.y = height/2 - second_player.size[2]/2
+  height = disp:getHeight()
+  width = disp:getWidth()
+  first_player = obj.Paddle(PAD_W, PAD_H)
+  first_player.x = 2
+  first_player.y = height/2 - first_player.size[2]/2
+  second_player = obj.Paddle(PAD_W, PAD_H)
+  second_player.x = width - 2 - 2
+  second_player.y = height/2 - second_player.size[2]/2
 
-    ball = obj.Ball(BALL_R)
-    ball.x = width/2
-    ball.y = height/2
-    ball.direction = {1, 0}
-    loopcnt = 0
-    old = tmr.now()
-    now = 0
-    fps = 0
+  ball = obj.Ball(BALL_R)
+  ball.x = width/2
+  ball.y = height/2
+  ball.direction = {1, 0}
+  loopcnt = 0
+  old = tmr.now()
+  now = 0
+  fps = 0
 end
 
 function update_game()
-    now = tmr.now()
-    local dt = now - old
-    --print(dt)
-    if dt >= ONE_SEC_US then
-        old = now
-        first_player.score = fps
-        fps = 0 
-    end
-    loopcnt = loopcnt + 1
-    if loopcnt > TEST_LOOPS then
-        tmr.stop(1)
-    end
-    ball.update(1)
-    --if (ball.x >  width-ball.r*2 or ball.x - ball.r < 0) then
-    --    ball.direction[1] = - ball.direction[1]
-    --elseif (ball.y >  height-ball.r*2 or ball.y - ball.r < 0) then
-    if (ball.y >  height-ball.r*2 or ball.y - ball.r < 0) then
-        ball.direction[2] = - ball.direction[2]
-    end
-    --print('update:', tmr.now()-now)
+  now = tmr.now()
+  local dt = now - old
+  --print(dt)
+  if dt >= ONE_SEC_US then
+    old = now
+    first_player.score = fps
+    fps = 0 
+  end
+  loopcnt = loopcnt + 1
+  if loopcnt > TEST_LOOPS then
+    tmr.stop(1)
+  end
+  ball.update(1)
+  --if (ball.x >  width-ball.r*2 or ball.x - ball.r < 0) then
+  --    ball.direction[1] = - ball.direction[1]
+  --elseif (ball.y >  height-ball.r*2 or ball.y - ball.r < 0) then
+  if (ball.y >  height-ball.r*2 or ball.y - ball.r < 0) then
+    ball.direction[2] = - ball.direction[2]
+  end
+  --print('update:', tmr.now()-now)
     
-    local vx = ball.direction[1]*ball.speed
-    local vy = ball.direction[2]*ball.speed
-    if ball.direction[1] > 0  then
-        if second_player.x <= ball.x + ball.r*2 and
-           second_player.x > ball.x - vx + ball.r*2 then
-           local collision_diff = ball.x + ball.r*2 - second_player.x
-           local k = collision_diff/vx
-           local y = vx*k + (ball.y - vy)
-           if y >= second_player.y and 
-            y + ball.r*2 <= second_player.y + second_player.size[2] then
-              ball.x = second_player.x - ball.r*2
-              ball.y = math.floor(ball.y - vy + vy*k)
-              ball.direction[1] = - ball.direction[1]
-            end
-        end 
-    else
-        if first_player.x + first_player.size[1] >= ball.x then
-           local collision_diff = first_player.x + first_player.size[1] - ball.x
-           local k = collision_diff/-vx
-           local y = vx*k + (ball.y - vy)
-           if y >= first_player.y and  
-            y + ball.r*2 <= first_player.y + second_player.size[2] then
-              ball.x = first_player.x - first_player.size[1]
-              ball.y = math.floor(ball.y - vy + vy*k)
-              ball.direction[1] = - ball.direction[1]   
-            end
-        end
-    end    
+  local vx = ball.direction[1]*ball.speed
+  local vy = ball.direction[2]*ball.speed
+  if ball.direction[1] > 0  then
+    if second_player.x <= ball.x + ball.r*2 and
+      second_player.x > ball.x - vx + ball.r*2 then
+      local collision_diff = ball.x + ball.r*2 - second_player.x
+      local k = collision_diff/vx
+      local y = vx*k + (ball.y - vy)
+      if y >= second_player.y and 
+        y + ball.r*2 <= second_player.y + second_player.size[2] then
+        ball.x = second_player.x - ball.r*2
+        ball.y = math.floor(ball.y - vy + vy*k)
+        ball.direction[1] = - ball.direction[1]
+      end
+    end 
+  else
+    if first_player.x + first_player.size[1] >= ball.x then
+      local collision_diff = first_player.x + first_player.size[1] - ball.x
+      local k = collision_diff/-vx
+      local y = vx*k + (ball.y - vy)
+      if y >= first_player.y and  
+      y + ball.r*2 <= first_player.y + second_player.size[2] then
+        ball.x = first_player.x - first_player.size[1]
+        ball.y = math.floor(ball.y - vy + vy*k)
+        ball.direction[1] = - ball.direction[1]   
+      end
+    end
+  end    
 end
 
 function draw_game()
-    first_player.draw()
-    second_player.draw()
-    -- ball
-    ball.draw()
-    -- score
-    disp:setFont(u8g.font_6x10)
-    disp:drawStr(width/2 - 10, height/8, tostring(first_player.score))
-    disp:drawStr(width/2 + 10, height/8, tostring(second_player.score))
-    -- frame
-    disp:drawFrame(0,0,width,height);
-    -- delay
-    local dot
-    for dot = 1, height, 16 do
-        disp:drawLine(width/2, dot, width/2, dot+10);
-    end
+  first_player.draw()
+  second_player.draw()
+  -- ball
+  ball.draw()
+  -- score
+  disp:setFont(u8g.font_6x10)
+  disp:drawStr(width/2 - 10, height/8, tostring(first_player.score))
+  disp:drawStr(width/2 + 10, height/8, tostring(second_player.score))
+  -- frame
+  disp:drawFrame(0,0,width,height);
+  -- delay
+  local dot
+  for dot = 1, height, 16 do
+    disp:drawLine(width/2, dot, width/2, dot+10);
+  end
 end
 
 init_i2c_display()
 init_game()
 
-tmr.alarm(1, 5, 1, function() 
-   update_game()
+tmr.alarm(1, TICKS, 1, function() 
+  update_game()
    
-   --local now_draw = tmr.now()
-   disp:firstPage()
-   repeat
+  --local now_draw = tmr.now()
+  disp:firstPage()
+  repeat
     draw_game()
-   until disp:nextPage() == false
-   --print("upd", tmr.now() - now_draw)
+  until disp:nextPage() == false
+  --print("upd", tmr.now() - now_draw)
    
-   tmr.wdclr()
-   fps = fps +1
-
+  tmr.wdclr()
+  fps = fps +1
 end )
--- pong_game/pong_objects.lua
+++ pong_game/pong_objects.lua
@@ -3,42 +3,42 @@ local M = {}
 _G[moduleName] = M
 
 local function Moveable()
-    local self = {
-        x = 0,
-        y = 0,
-        speed = 2,
-        direction = {0, 0}
-    }
-    function self.update()
-    end
-    function self.draw()
-    end
-    return self
+  local self = {
+    x = 0,
+    y = 0,
+    speed = 2,
+    direction = {0, 0}
+  }
+  function self.update()
+  end
+  function self.draw()
+  end
+  return self
 end
 
 local function Paddle(w, h)
-    local self = Moveable()
-    self.size = {w, h}
-    self.score = 0
+  local self = Moveable()
+  self.size = {w, h}
+  self.score = 0
 
-    function self.draw()
-        disp:drawBox(self.x, self.y, self.size[1], self.size[2])
-    end
-    
-    return self
+  function self.draw()
+    disp:drawBox(self.x, self.y, self.size[1], self.size[2])
+  end
+  
+  return self
 end
 
 local function Ball(r)
-    local self = Moveable()
-    self.r = r
-    function self.update(dt)
-        self.x = self.x + self.speed*self.direction[1]*dt
-        self.y = self.y + self.speed*self.direction[2]*dt
-    end
-    function self.draw()
-        disp:drawDisc(self.x, self.y, self.r)
-    end
-    return self
+  local self = Moveable()
+  self.r = r
+  function self.update(dt)
+    self.x = self.x + self.speed*self.direction[1]*dt
+    self.y = self.y + self.speed*self.direction[2]*dt
+  end
+  function self.draw()
+    disp:drawDisc(self.x, self.y, self.r)
+  end
+  return self
 end
 
 M.Paddle = Paddle
