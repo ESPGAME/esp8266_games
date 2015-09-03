538efc4bf4dda592e4b705c5489d09bacb5d6ec4
Alexander <sash13@bigmir.net>
2015-09-03 22:01:40 +0300

    Update xo

---
+++ common/game_player.lua
@@ -0,0 +1,13 @@
+local function Player()
+  local self = {
+    x = 0,
+    y = 0,
+    speed = 2,
+    direction = {0, 0}
+  }
+  function self.update()
+  end
+  function self.draw()
+  end
+  return self
+end
--- xo_game/xo_game.lua
+++ xo_game/xo_game.lua
@@ -7,27 +7,35 @@ local function XO()
   local LINE = ONE_CELL*3
   local DELAY_X = (width-LINE)/2
   local DELAY_Y = (height-LINE)/2
+  local X = 1
+  local O = 2
+  local EMPTY = 0
   local idx = {1, 1}
+  local cells = {0, 1, 0, 1, 0, 0, 0, 0, 2}
 
-  local function set_x(x, y)
-    local x_sel = DELAY_X+ONE_CELL*(x-1) + 1
-    local y_sel = DELAY_Y+ONE_CELL*(y-1) + 1
-    disp:drawLine(x_sel, y_sel, x_sel+ONE_CELL-2, y_sel+ONE_CELL-2);
-    disp:drawLine(x_sel, y_sel+ONE_CELL-2, x_sel+ONE_CELL-2, y_sel);
-  end
-
-  local function set_o(x, y)
-    local x_sel = DELAY_X+ONE_CELL*(x-1) + ONE_CELL/2
-    local y_sel = DELAY_Y+ONE_CELL*(y-1) + ONE_CELL/2
-    disp:drawCircle(x_sel, y_sel, (ONE_CELL-4)/2)
+  local function set(x, y, t)
+    local x_sel = DELAY_X+ONE_CELL*x
+    local y_sel = DELAY_Y+ONE_CELL*y
+    if t == EMPTY then
+      return
+    elseif t == X then
+      x_sel = x_sel + 1
+      y_sel = y_sel + 1
+      disp:drawLine(x_sel, y_sel, x_sel+ONE_CELL-2, y_sel+ONE_CELL-2);
+      disp:drawLine(x_sel, y_sel+ONE_CELL-2, x_sel+ONE_CELL-2, y_sel);
+    elseif t == O then
+      x_sel = x_sel + ONE_CELL/2
+      y_sel = y_sel + ONE_CELL/2
+      disp:drawCircle(x_sel, y_sel, (ONE_CELL-4)/2)
+    end
   end
 
   function self.update(dt)
-    -- check cursor
-    if idx[1] > 3 then idx[1] = 1 end
-    if idx[2] > 3 then idx[2] = 1 end
-    if idx[1] < 1 then idx[1] = 3 end
-    if idx[2] < 1 then idx[2] = 3 end
+    -- check cursor bound
+    if idx[1] > 2 then idx[1] = 0 end
+    if idx[2] > 2 then idx[2] = 0 end
+    if idx[1] < 0 then idx[1] = 2 end
+    if idx[2] < 0 then idx[2] = 2 end
   end
 
   function self.draw()
@@ -37,22 +45,18 @@ local function XO()
     -- h lines
     disp:drawLine(DELAY_X, DELAY_Y+ONE_CELL, DELAY_X+LINE, DELAY_Y+ONE_CELL);
     disp:drawLine(DELAY_X, DELAY_Y+ONE_CELL*2, DELAY_X+LINE, DELAY_Y+ONE_CELL*2);
-    -- test selector
-    local x_sel = DELAY_X*idx[1] + 1
-    local y_sel = DELAY_Y*idx[2] + 1
+    -- draw selector
+    local x_sel = DELAY_X+ONE_CELL*idx[1] + 1
+    local y_sel = DELAY_Y+ONE_CELL*idx[2] + 1
     disp:drawFrame(x_sel,y_sel,ONE_CELL-2,ONE_CELL-2);
-    -- test xo
-    set_o(1, 1)
-    set_o(2, 3)
-    set_x(2, 1)
-    set_x(2, 2)
-    set_o(1, 2)
-    set_x(3, 3)
-    set_x(3, 1)
-    set_o(3, 2)
-    set_o(1, 3)
+    -- draw xo
+    for x = 0, 2 do
+      for y = 0, 2 do
+        set(x, y, cells[x+y*3+1])
+      end
+    end
   end
-
+  
   return self
 end
 
